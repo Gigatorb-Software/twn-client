67e66fbec61128422b0afc8e1e36015f7de877a9
Harsh Rathod <hrathore50@ymail.com>
2022-05-28 00:54:08 +0530

    Refactored code and fixed lint issues.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import type { ReactElement } from "react";
 
-function App() {
+const App = (): ReactElement => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
