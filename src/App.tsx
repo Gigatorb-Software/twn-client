6ba2e9bfaca9ab55a711c434a908cd0e7002db6c
Harsh Rathod <hrathore50@ymail.com>
2022-05-28 11:15:47 +0530

    Integrated React Router v6.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "./logo.svg";
 import type { ReactElement } from "react";
 
 const App = (): ReactElement => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
 
