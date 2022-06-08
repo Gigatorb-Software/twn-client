20062e1e82a40e24d56d663b06b43381d185f6af
nazim <nazimkhan71999@gmail.com>
2022-06-08 17:25:17 +0530

    Initial commit.

---
+++ Desktop/vivek/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Container } from '@mui/material';
+
+import gigatorb1 from './component/gigatorb1';
+
+const App = () => {
+  return (
+    <Container>
+    
+    {gigatorb1()}
+      
+    </Container>
+  );
+}
+
+
+
+export default App;
